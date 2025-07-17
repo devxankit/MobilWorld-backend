9960b64c7c2c0b7cc360faf338cb5bbede600b94
raihandevelopers <raihankhan.indore@gmail.com>
2025-07-17 14:49:46 +0530

    new

--- middleware/upload.js
+++ middleware/upload.js
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dj6z5fgff',
+  api_key: '336794359142348',
+  api_secret: '9WDGjGOJ9PqhWJHKrgG0Dp4PCpI',
 });
 
 // Custom storage for Cloudinary
